02fb94d75e801d8aed060bd63ad805b04a637451
Barduino <barduinor@gmail.com>
2018-08-18 17:43:45 -0400

    aaa

--- scripts/powershell-core/DeployLatestTagsToTargetEnv.ps1
+++ scripts/powershell-core/DeployLatestTagsToTargetEnv.ps1
@@ -16,44 +16,58 @@
 <###################################################################################>
 function CallDeploymentAPI ($Method, $Endpoint, $Body)
 {
-	#$Url = "https://$env:LifeTimeUrl/LifeTimeAPI/rest/v1/$Endpoint"
-	$Url = "https://catalyst-lt.outsystemsenterprise.com/LifeTimeAPI/rest/v1/$Endpoint"
+        $Url = "https://$env:LifeTimeUrl/LifeTimeAPI/rest/v1/$Endpoint"
+        #$Url = "https://catalyst-lt.outsystemsenterprise.com/LifeTimeAPI/rest/v1/$Endpoint"
 
     $ContentType = "application/json"
-	$Headers = @{
-		#Authorization = "Bearer $env:AuthorizationToken"
+        $Headers = @{
+                #Authorization = "Bearer $env:AuthorizationToken"
         Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJsaWZldGltZSIsInN1YiI6IllqUmtZbU00WkRjdE9EWTVaQzAwTWpWaUxUbGhaRGd0WldZM05HVmlOV1ZsWVRRMSIsImF1ZCI6ImxpZmV0aW1lIiwiaWF0IjoiMTUzNDM2NDUzNyIsImppdCI6Ik0xT20xVXBqbloifQ==.ZU0vGrIQtiaMOXYFDSR/+Lp6Fd14aZKFvHW1mLsNEhI="
-		Accept = "application/json"
-	}
-		
-	try { Invoke-RestMethod -Method $Method -Uri $Url -Headers $Headers -ContentType $ContentType -Body $body }
-	catch { Write-Host $_; exit 9 }
-}	
+                Accept = "application/json"
+        }
 
-# Load CSV from file
-#$csv_env = import-csv -Path /tmp/LT.Environments.csv.mapping
-#$csv_app = import-csv -Path /tmp/LT.Applications.csv.mapping
-$csv_env = import-csv -Path $env:WORKSPACE/LT.Environments.csv.mapping
-$csv_app = import-csv -Path $env:WORKSPACE/LT.Applications.csv.mapping
+        try { Invoke-RestMethod -Method $Method -Uri $Url -Headers $Headers -ContentType $ContentType -Body $body }
+        catch { Write-Host $_; exit 9 }
+}
+
+# Unable to load from csv of another session
+# get files from api again
+$Environments = CallDeploymentAPI -Method GET -Endpoint environments
+
+# Process output
+$envtab = $Environments | Select-Object -Property Name,Key
+$csv_env = $envtab
+
+
+# Fetch latest OS Applications data
+# Unable to load CSV file form another session
+# get applications from api again
+$Applications = CallDeploymentAPI -Method GET -Endpoint applications
+
+# Process output
+$appstab = $Applications  | Select-Object -Property Name,Key
+$csv_app = $appstab
 
 # Translate environment names to the corresponding keys
 
-#$SourceEnvKey = $csv_env.Where({$_.name -eq "Development"}).key   
-#$TargetEnvKey = $csv_env.Where({$_.name -eq "Production"}).key 
-$SourceEnvKey = $csv_env.Where({$_.name -eq "$env:SourceEnvironment"}).key   
-$TargetEnvKey = $csv_env.Where({$_.name -eq "$env:TargetEnvironment"}).key   
+#$SourceEnvKey = $csv_env.Where({$_.name -eq "Development"}).key
+#$TargetEnvKey = $csv_env.Where({$_.name -eq "Production"}).key
+$SourceEnvKey = $csv_env.Where({$_.name -eq "$env:SourceEnvironment"}).key
+$TargetEnvKey = $csv_env.Where({$_.name -eq "$env:TargetEnvironment"}).key
 
 
 
 # Translate application names to the corresponding keys
-$apps = "Azure Cognitive Services Connector,Barcode Plugin,Business Activity Monitoring" -split ","
-#$apps = $env:ApplicationsToDeploy
+$apps = $env:ApplicationsToDeploy -split ","
+
 
 $AppKeys = ""
 foreach($item in $apps){
     $Appkeys = $Appkeys + "," + ($csv_app.Where({$_.Name -eq $item}).Key)
     }
-$AppKeys = $AppKeys.subsstring(1)
+$AppKeys = $AppKeys.substring(1)
+
+
 
 echo "Creating deployment plan from '$env:SourceEnvironment' ($SourceEnvKey) to '$env:TargetEnvironment' ($TargetEnvKey) including applications: $env:ApplicationsToDeploy ($AppKeys)."
 
@@ -63,10 +77,10 @@ $AppVersionKeys = ( $AppKeys -split "," | %{ CallDeploymentAPI -Method GET -Endp
 # Create a new LifeTime Deployment Plan that includes the retrieved version Tags
 $RequestBody = @"
 {
-	"ApplicationVersionKeys": [$AppVersionKeys],
-	"Notes" : "Automatic deployment plan created by Jenkins",
-	"SourceEnvironmentKey":"$SourceEnvKey",
-	"TargetEnvironmentKey":"$TargetEnvKey"
+        "ApplicationVersionKeys": [$AppVersionKeys],
+        "Notes" : "Automatic deployment plan created by Jenkins",
+        "SourceEnvironmentKey":"$SourceEnvKey",
+        "TargetEnvironmentKey":"$TargetEnvKey"
 }
 "@
 
@@ -80,21 +94,21 @@ echo "Deployment plan '$DeploymentPlanKey' started being executed."
 # Sleep thread until deployment has finished
 $WaitCounter = 0
 do {
-	Start-Sleep -s $env:SleepPeriodInSecs
-	$WaitCounter += $env:SleepPeriodInSecs
-	echo "$WaitCounter secs have passed since the deployment started..."	
-	
-	# Check Deployment Plan status. If deployment is still running then go back to step 5
-	$DeploymentStatus =  CallDeploymentAPI -Method GET -Endpoint "deployments/$DeploymentPlanKey/status" | %{ $_.DeploymentStatus }
-	
-	if ($DeploymentStatus -ne "running") {	
-		# Return Deployment Plan status
-		echo "Deployment plan finished with status '$DeploymentStatus'."
-		exit 0
-	}
+        Start-Sleep -s $env:SleepPeriodInSecs
+        $WaitCounter += $env:SleepPeriodInSecs
+        echo "$WaitCounter secs have passed since the deployment started..."
+
+        # Check Deployment Plan status. If deployment is still running then go back to step 5
+        $DeploymentStatus =  CallDeploymentAPI -Method GET -Endpoint "deployments/$DeploymentPlanKey/status" | %{ $_.DeploymentStatus }
+
+        if ($DeploymentStatus -ne "running") {
+                # Return Deployment Plan status
+                echo "Deployment plan finished with status '$DeploymentStatus'."
+                exit 0
+        }
 }
 while ($WaitCounter -lt $env:DeploymentTimeoutInSecs)
 
-# Deployment timeout reached. Exit script with error  
+# Deployment timeout reached. Exit script with error
 echo "Timeout occurred while deployment plan is still in 'running' status."
 exit 1
