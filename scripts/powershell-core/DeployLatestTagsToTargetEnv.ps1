bdb9e139a18d4f7421529c74a9636bf8e2c6cc4b
Barduino <barduinor@gmail.com>
2018-08-18 14:34:42 -0400

    initial commit

---
+++ scripts/powershell-core/DeployLatestTagsToTargetEnv.ps1
@@ -0,0 +1,100 @@
+<###################################################################################>
+<#       Script: DeployLatestTagsToTargetEnv                                       #>
+<#  Description: Deploy to target environment the latest tags of configured        #>
+<#               LifeTime applications.                                            #>
+<#         Date: 2017-10-06                                                        #>
+<#       Author: rrmendes                                                          #>
+<#         Path: jenkins/scripts/powershell/DeployLatestTagsToTargetEnv.ps1        #>
+<###################################################################################>
+
+<###################################################################################>
+<#     Function: CallDeploymentAPI                                                 #>
+<#  Description: Helper function that wraps calls to the LifeTime Deployment API.  #>
+<#       Params: -Method: HTTP Method to use for API call                          #>
+<#               -Endpoint: Endpoint of the API to invoke                          #>
+<#               -Body: Request body to send when calling the API                  #>
+<###################################################################################>
+function CallDeploymentAPI ($Method, $Endpoint, $Body)
+{
+	#$Url = "https://$env:LifeTimeUrl/LifeTimeAPI/rest/v1/$Endpoint"
+	$Url = "https://catalyst-lt.outsystemsenterprise.com/LifeTimeAPI/rest/v1/$Endpoint"
+
+    $ContentType = "application/json"
+	$Headers = @{
+		#Authorization = "Bearer $env:AuthorizationToken"
+        Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJsaWZldGltZSIsInN1YiI6IllqUmtZbU00WkRjdE9EWTVaQzAwTWpWaUxUbGhaRGd0WldZM05HVmlOV1ZsWVRRMSIsImF1ZCI6ImxpZmV0aW1lIiwiaWF0IjoiMTUzNDM2NDUzNyIsImppdCI6Ik0xT20xVXBqbloifQ==.ZU0vGrIQtiaMOXYFDSR/+Lp6Fd14aZKFvHW1mLsNEhI="
+		Accept = "application/json"
+	}
+		
+	try { Invoke-RestMethod -Method $Method -Uri $Url -Headers $Headers -ContentType $ContentType -Body $body }
+	catch { Write-Host $_; exit 9 }
+}	
+
+# Load CSV from file
+#$csv_env = import-csv -Path /tmp/LT.Environments.csv.mapping
+#$csv_app = import-csv -Path /tmp/LT.Applications.csv.mapping
+$csv_env = import-csv -Path $env:WORKSPACE/LT.Environments.csv.mapping
+$csv_app = import-csv -Path $env:WORKSPACE/LT.Applications.csv.mapping
+
+# Translate environment names to the corresponding keys
+
+#$SourceEnvKey = $csv_env.Where({$_.name -eq "Development"}).key   
+#$TargetEnvKey = $csv_env.Where({$_.name -eq "Production"}).key 
+$SourceEnvKey = $csv_env.Where({$_.name -eq "$env:SourceEnvironment"}).key   
+$TargetEnvKey = $csv_env.Where({$_.name -eq "$env:TargetEnvironment"}).key   
+
+
+
+# Translate application names to the corresponding keys
+$apps = "Azure Cognitive Services Connector,Barcode Plugin,Business Activity Monitoring" -split ","
+#$apps = $env:ApplicationsToDeploy
+
+$AppKeys = ""
+foreach($item in $apps){
+    $Appkeys = $Appkeys + "," + ($csv_app.Where({$_.Name -eq $item}).Key)
+    }
+$AppKeys = $AppKeys.subsstring(1)
+
+echo "Creating deployment plan from '$env:SourceEnvironment' ($SourceEnvKey) to '$env:TargetEnvironment' ($TargetEnvKey) including applications: $env:ApplicationsToDeploy ($AppKeys)."
+
+# Get latest version Tags for each OS Application to deploy
+$AppVersionKeys = ( $AppKeys -split "," | %{ CallDeploymentAPI -Method GET -Endpoint "applications/$_/versions?MaximumVersionsToReturn=1" } | %{ '"' + $_.Key + '"' } ) -join ","
+
+# Create a new LifeTime Deployment Plan that includes the retrieved version Tags
+$RequestBody = @"
+{
+	"ApplicationVersionKeys": [$AppVersionKeys],
+	"Notes" : "Automatic deployment plan created by Jenkins",
+	"SourceEnvironmentKey":"$SourceEnvKey",
+	"TargetEnvironmentKey":"$TargetEnvKey"
+}
+"@
+
+$DeploymentPlanKey = CallDeploymentAPI -Method POST -Endpoint "deployments" -Body $RequestBody
+echo "Deployment plan '$DeploymentPlanKey' created successfully."
+
+# Start Deployment Plan execution
+$DeploymentPlanStart = CallDeploymentAPI -Method POST -Endpoint "deployments/$DeploymentPlanKey/start"
+echo "Deployment plan '$DeploymentPlanKey' started being executed."
+
+# Sleep thread until deployment has finished
+$WaitCounter = 0
+do {
+	Start-Sleep -s $env:SleepPeriodInSecs
+	$WaitCounter += $env:SleepPeriodInSecs
+	echo "$WaitCounter secs have passed since the deployment started..."	
+	
+	# Check Deployment Plan status. If deployment is still running then go back to step 5
+	$DeploymentStatus =  CallDeploymentAPI -Method GET -Endpoint "deployments/$DeploymentPlanKey/status" | %{ $_.DeploymentStatus }
+	
+	if ($DeploymentStatus -ne "running") {	
+		# Return Deployment Plan status
+		echo "Deployment plan finished with status '$DeploymentStatus'."
+		exit 0
+	}
+}
+while ($WaitCounter -lt $env:DeploymentTimeoutInSecs)
+
+# Deployment timeout reached. Exit script with error  
+echo "Timeout occurred while deployment plan is still in 'running' status."
+exit 1
